d570149a8c1d1c92b81140506410a2b717a402ca
Destroyer <32405118+DestroyerDarkNess@users.noreply.github.com>
2022-08-21 09:24:57 -0400

    Up

---
+++ GameExitFix/DllMain.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class DLLMain
+
+    <UnmanagedFunctionPointer(CallingConvention.Cdecl)>
+    Public Delegate Sub SAMP_ChatDelegate(ByVal ChatEntered As String)
+
+    <DllImport("ChatHostHandler.asi", CallingConvention:=CallingConvention.Cdecl)>
+    Public Shared Sub SetChatCallback(ByVal aCallback As SAMP_ChatDelegate)
+    End Sub
+
+    Public Shared Sub DllMain()
+        Try
+
+            Dim SAMP_Chat_Thread As SAMP_ChatDelegate = New SAMP_ChatDelegate(AddressOf MainCheat)
+            SetChatCallback(SAMP_Chat_Thread)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Exit Sub
+        End Try
+
+
+        Do While True : Loop
+
+    End Sub
+
+    Public Shared Sub MainCheat(ByVal ChatEnteredEx As String)
+        If ChatEnteredEx.ToLower = "/qq" Then
+            Process.GetCurrentProcess.Kill()
+        End If
+    End Sub
+
+End Class
---
+++ GameExitFix/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GameExitFix")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GameExitFix")> 
+<Assembly: AssemblyCopyright("Copyright ©  2022")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("98d12850-323f-4093-8e06-73abe1a3e440")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
